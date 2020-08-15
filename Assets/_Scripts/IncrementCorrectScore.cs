cf6d3c50aa3b7483d5e14b6002c5201df2b67e4c
dr-matt-smith <matt.smith@itb.ie>
2020-08-15 13:21:45 +0100

    added files to project

---
+++ Assets/_Scripts/IncrementCorrectScore.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class IncrementCorrectScore : MonoBehaviour
+{
+	void Start () {
+		int newScoreCorrect = 1 + PlayerPrefs.GetInt("scoreCorrect");
+		PlayerPrefs.SetInt("scoreCorrect", newScoreCorrect);
+	} 
+}
